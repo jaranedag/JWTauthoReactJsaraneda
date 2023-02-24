7e85291ba0f7ed604cf284b5cbf1d964d56cfd2e
jaranedag <jaranedag@udd.cl>
2023-02-24 14:57:28 +0000

    jwt reactjs javier araneda

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../../styles/home.css'
 
 export const Navbar = () => {
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+		<div className="mb-0">
+			<nav className="navbar navbar-expand-lg  navbarColor textoTamañoMediano px-0 mb-0">
+				<nav className="navbar ps-3">
+					<div className="container-fluid">
+						<Link to="/" className="text-decoration-none">
+							<i className="fa-solid   navbarFontColor textoTamañoGrande"></i><a className="navbar-brand ps-2 navbarFontColor textoTamañoGrande" href="#">HOME</a>
+						</Link>
+					</div>
+				</nav>
+				<ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+					<li className="nav-item">
+						<Link to="/recetas" className="text-decoration-none">
+							<a className="nav-link active navbarFontColor" aria-current="page" href="#"></a>
+						</Link>
+					</li>
+					<li className="nav-item">
+					<Link to="/entrenamiento" className="text-decoration-none">
+						<a className="nav-link navbarFontColor" href="#"></a>
+						</Link>
+					</li>
+					{/*<li className="nav-item">
+						<Link to="/tusemana" className="text-decoration-none">
+							<a className="nav-link navbarFontColor" href="#">Tu semana</a>
+						</Link>
+	                </li>*/}
+				</ul>
+				{/* login icon */}
+				<li className="navbar-nav px-1">
+					<Link to="/vistausuario" className="text-decoration-none">
+						<i className="fa-regular fa-circle-user  navbarFontColor textoTamañoGrande"></i>
 					</Link>
-				</div>
-			</div>
-		</nav>
+				</li>
+
+
+				<li className="navbar-nav pe-3 ps-1">
+					<Link to="/login" className="text-decoration-none">
+						<a className="nav-link  navbarFontColor" href="#">Login</a>
+					</Link>
+					<Link to="/signup" className="text-decoration-none">
+						<a className="nav-link  navbarFontColor" href="#">Signup</a>
+					</Link>
+				</li>
+				{/* logout icon */}
+
+
+				<li className="navbar-nav px-3" >
+
+
+					<Link to="/" className="text-decoration-none">
+						
+						<i className="fa-solid fa-arrow-right-from-bracket tamañotextoGrande"></i>
+					</Link>
+				</li>
+			</nav>
+			<div className="linea-azul-navbar"></div>
+		</div>
 	);
 };
